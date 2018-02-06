d28f7caea2c10105895ecc69c996b145dc1cc0d3
Mitch Lamers <mitchlamers@gmail.com>
2018-02-06 18:08:24 +0100

    module 7

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -81,3 +81,15 @@ exports.getStoreBySlug = async (req, res, next) => {
     if (!store) return next();
     res.render('store', { store })
 }
+
+exports.getStoresByTag = async (req, res) => {
+    const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true };
+    const tagsPromise = Store.getTagsList();
+    const storesPromise = Store.find({ tags: tagQuery })
+    const [tags, stores] = await Promise.all([
+        tagsPromise,
+        storesPromise
+    ])
+    res.render('tag', { tags, title: 'Tags', tag, stores });
+};
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -24,4 +24,7 @@ router.post('/add',
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
 module.exports = router;
